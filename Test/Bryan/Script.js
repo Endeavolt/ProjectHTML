8157cb96a6dec56c589b8a2cb64cefc68ab274b9
Endeavolt <bryanbacheletpro@gmailcom>
2021-06-30 22:33:17 +0200

    Function JS

--- Test/Bryan/Script.js
+++ Test/Bryan/Script.js
@@ -1,6 +1,33 @@
+$(document).ready(function(){
+  $(window).scroll(function(){
+    if(this.scrollY>20){
+      $('.navbar').addClass("sticky");
+  }else{
+      $('.navbar').removeClass("sticky");
+  }
+      if(this.scrollY > 500){
+          $('.btn').addClass("show");
+      }else{
+          $('.btn').removeClass("show");
+      }
+  });
+  
+  // slide-up script
+  $('.btn').click(function(){
+      $('html').animate({scrollTop : 0})
+  });
+
+});
+
+
+
+
+
+
+
 function initMap() {
     // The location of Uluru
-    const uluru = { lat: -20.344, lng: 131.036 };
+    const uluru = { lat: -20.344, lng: 1.036 };
     // The map, centered at Uluru
     const map = new google.maps.Map(document.getElementById("map"), {
       zoom: 4,
