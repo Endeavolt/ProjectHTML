b914b9aa370c7ebb1097c99931afd86e7be97754
Endeavolt <bryanbacheletpro@gmailcom>
2021-07-12 19:25:34 +0200

    Polish Version Dev

--- Test/Bryan/Script.js
+++ Test/Bryan/Script.js
@@ -22,12 +22,15 @@ $(document).ready(function(){
 
 
 
+function button(){
+  console.log("Button Click");
 
+}
 
 
 function initMap() {
     // The location of Uluru
-    const uluru = { lat: -20.344, lng: 1.036 };
+    const uluru = { lat: 47.670722, lng: -2.960611 };
     // The map, centered at Uluru
     const map = new google.maps.Map(document.getElementById("map"), {
       zoom: 4,
