c6bd511ed83a42be55f4f1b47a7ba7092d99e46b
Bryan Bachelet <bryanbacheletpro@gmailcom>
2022-03-02 10:38:46 +0100

    Finish Section Info + Navbar Working on the comment section

--- Test/Bryan/Script.js
+++ Test/Bryan/Script.js
@@ -1,24 +1,41 @@
-$(document).ready(function(){
-  $(window).scroll(function(){
-    if(this.scrollY>20){
-      $('.navbar').addClass("sticky");
-  }else{
-      $('.navbar').removeClass("sticky");
-  }
-      if(this.scrollY > 500){
-          $('.btn').addClass("show");
-      }else{
-      }
-    
-  });
-  
 
-   /*Responsive function */
-  $('.menu-btn').click(function()
-  {
-     $('.navbar .menu').toggleClass("active");
-     $('.exit').toggleClass("active");
+var test = Boolean(false);
+
+function toggleBoolean(s) {
+  s.state = !s.state;
+}
+
+$(document).ready(function () {
+  $(window).scroll(function () {
+    if (this.scrollY > 20) {
+      $('.navbar').addClass("sticky");
+    } else {
+      $('.navbar').removeClass("sticky");
+    }
+    if (this.scrollY > 500) {
+      $('.btn').addClass("show");
+    } else {
+    }
+
+  });
+
+
+  /*Responsive function */
+  $('.menu-btn').click(function () {
+    $('.navbar .menu').toggleClass("active");
+    $('.exit').toggleClass("active");
   })
+
+
+  $('.info .max-width .left .week .discover').click(function () {
+    console.log(test);
+    if (!test) {
+      test = true
+      $('.info .max-width .left .horaire ').toggleClass("active");
+      $('.info .max-width .left .week .discover ').toggleClass("active");
+    }
+  })
+
 
 });
 
@@ -43,25 +60,25 @@ $(document).ready(function(){
 
 // Email Sending 
 
-(function() {
+(function () {
   // https://dashboard.emailjs.com/admin/integration
   emailjs.init("user_FOtjYlbaUcPGjYFU2V7vN");
 })();
 
-window.onload = function() {
-  document.getElementById('contact-form').addEventListener('submit', function(event) {
-      event.preventDefault();
-      // generate a five digit number for the contact_number variable
-    
-      // these IDs from the previous steps
-      emailjs.sendForm('service_byk8y77', 'template_7iqzmc7', this)
-          .then(function() {
-              console.log('SUCCESS!');
-              document.getElementById('contact-form').reset();
-          }, function(error) {
-              console.log('FAILED...', error);
-              document.getElementById('contact-form').reset();
-          });
+window.onload = function () {
+  document.getElementById('contact-form').addEventListener('submit', function (event) {
+    event.preventDefault();
+    // generate a five digit number for the contact_number variable
+
+    // these IDs from the previous steps
+    emailjs.sendForm('service_byk8y77', 'template_7iqzmc7', this)
+      .then(function () {
+        console.log('SUCCESS!');
+        document.getElementById('contact-form').reset();
+      }, function (error) {
+        console.log('FAILED...', error);
+        document.getElementById('contact-form').reset();
+      });
   });
 }
 
